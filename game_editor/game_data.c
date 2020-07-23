afffdea80ccc3acdad2b3321c304c90f3fabf552
anthony <anthony@localhost>
2020-07-23 20:04:06 +0100

    editor load game data

--- game_editor/game_data.c
+++ game_editor/game_data.c
@@ -17,22 +17,26 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "game_data.h"
 
 static inline void 
-cpyname(char *dst, char *src)
+cpyname(char *dst, char *src, int max)
 {
-	strncpy(dst, src, MAX_NAME);
+	strncpy(dst, src, max);
 	dst[MAX_NAME - 1] = '\0';
 }
 
 struct entity *
-new_entity(char *name, int weight) 
+new_entity(char *name, char *desc, char icon, int weight, enum size_type size_class)
 {
 	struct entity *entity = malloc(sizeof(struct entity));
-	cpyname(entity->name, name);
+	cpyname(entity->name, name, MAX_NAME);
 	entity->weight = weight;
+	cpyname(entity->desc, desc, MAX_DESC);
+	entity->icon = icon;
+	entity->size_class = size_class;
 	return entity;
 }
 
@@ -40,17 +44,111 @@ struct ground *
 new_ground(char *name, char icon)
 {
 	struct ground *ground = malloc(sizeof(struct ground));
-	cpyname(ground->name, name);
+	cpyname(ground->name, name, MAX_NAME);
 	ground->icon = icon;
 	return ground;
 }
 
 
 struct creature *
-new_creature(char *name, int health)
+new_creature(char *name, char *desc, int health, int tp, int inventory_size)
 {
 	struct creature *creature = malloc(sizeof(struct creature));
-	cpyname(creature->name, name);
+	cpyname(creature->name, name, MAX_NAME);
 	creature->health = health;
+	cpyname(creature->desc, desc, MAX_DESC);
+	creature->tp = tp;
+	creature->inventory_size = inventory_size;
 	return creature;
+}
+
+
+char *
+sizestr(enum size_type type)
+{
+	switch (type) {
+		case SIZE_HUGE:
+			return "SIZE_HUGE";
+		case SIZE_LARGE:
+			return "SIZE_LARGE";
+		case SIZE_SMALL:
+			return "SIZE_SMALL";
+		case SIZE_TINY:
+			return "SIZE_TINY";
+	}
+}
+
+void 
+save_game_data(char *outpath, struct entity *entities, struct ground *grounds, struct creature *creatures)
+{
+	FILE *file = fopen(outpath, "r");
+}
+
+enum size_type 
+entity_size(char *string)
+{
+	int len = strlen(string);
+	if (strncmp(string, "SIZE_HUGE", len) == 0) {
+		return SIZE_HUGE;
+	} else if (strncmp(string, "SIZE_LARGE", len) == 0) {
+		return SIZE_LARGE;
+	} else if (strncmp(string, "SIZE_SMALL", len) == 0) {
+		return SIZE_SMALL;
+	} else if (strncmp(string, "SIZE_TINY", len) == 0) {
+		return SIZE_TINY;
+	} else {
+		return SIZE_TINY;
+	}
+}
+void 
+load_game_data(char *inpath, struct entity ***edest, struct ground ***gdest, struct creature ***cdest)
+{
+	int ecount, gcount, ccount, scount;
+	char linebuff[MAX_LINE];
+	FILE *file = fopen(inpath, "r");
+	
+	while (*fgets(linebuff, MAX_LINE, file) != '\t')
+		ecount++;
+	while (*fgets(linebuff, MAX_LINE, file) != '\t')
+		gcount++;
+	while (*fgets(linebuff, MAX_LINE, file) != '\t')
+		ccount++;
+	while (*fgets(linebuff, MAX_LINE, file) != '\t')
+		scount++;
+	
+	struct entity **ents = malloc(ecount * sizeof(struct entity*));
+	struct ground **grounds = malloc(gcount * sizeof(struct ground*));
+	struct creature **creatures = malloc(ccount * sizeof(struct creatures*));
+
+	fseek(file, 0, SEEK_SET);
+	int i = 0;
+	char *line;
+	while (*(line = fgets(linebuff, MAX_LINE, file)) != '\t') {
+		printf("loading entity %d with data '%s'\n", i, line);
+		char *name = strtok(line, "\t");
+		char *icon = strtok(NULL, "\t");
+		int weight = (int)strtol(strtok(NULL, "\t"), NULL, 10);
+		enum size_type size = entity_size(strtok(NULL, "\t"));
+		ents[i++] = new_entity(name, "No description", *icon, weight, size);
+	}
+	i = 0;
+	while (*(line = fgets(linebuff, MAX_LINE, file)) != '\t') {
+		printf("loading ground %d with data '%s'\n", i, line);
+		char *name = strtok(line, "\t");
+		char *icon = strtok(NULL, "\t");
+		grounds[i++] = new_ground(name, *icon);
+	}
+	i = 0;
+	while (*(line = fgets(linebuff, MAX_LINE, file)) != '\t') {
+		printf("loading creature %d with data '%s'\n", i, line);
+		char *name = strtok(line, "\t");
+		int health = (int)strtol(strtok(NULL, "\t"), NULL, 10);
+		int tp = (int)strtol(strtok(NULL, "\t"), NULL, 10);
+		int inventory_size = (int)strtol(strtok(NULL, "\t"), NULL, 10);
+		creatures[i++] = new_creature(name, "No description", health, tp, inventory_size);
+	}
+	
+	*edest = ents;
+	*gdest = grounds;
+	*cdest = creatures;
 }
--- game_editor/game_data.h
+++ game_editor/game_data.h
@@ -19,21 +19,40 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define _ENTITY_H
 
 #define MAX_NAME 10
+#define MAX_DESC 100
+#define MAX_LINE 200
 
+enum size_type {
+	SIZE_TINY,
+	SIZE_SMALL,
+	SIZE_LARGE,
+	SIZE_HUGE
+};
 struct entity {
 	char name[MAX_NAME];
+	char desc[MAX_DESC];
+	char icon;
 	int weight;
+	enum size_type size_class;
 };
 struct creature {
 	char name[MAX_NAME];
+	char desc[MAX_DESC];
 	int health;
+	int tp;
+	int inventory_size;
 };
 struct ground {
 	char name[MAX_NAME];
 	char icon;
 };
 
-struct entity *new_entity(char *name, int weight);
+struct entity *new_entity(char *name, char *desc, char icon, int weight, enum size_type size_class);
 struct ground *new_ground(char *name, char icon);
-struct creature *new_creature(char *name, int health);
+struct creature *new_creature(char *name, char *desc, int health, int tp, int inventory_size);
+
+// Expects null terminated arrays
+void save_game_data(char *outpath, struct entity *entities, struct ground *grounds, struct creature *creatures);
+// Allocates memory
+void load_game_data(char *inpath, struct entity ***edest, struct ground ***gdest, struct creature ***cdest);
 #endif
