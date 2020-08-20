1d6009ccadb9714a151acd02036fa92e70714a36
anthony <anthony@localhost>
2020-08-20 19:26:42 +0100

    update gitignore

--- build.sh
+++ build.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-cc *.c  -g -lm -o main
+cd game_editor
+./build.sh
+cd ..
 
+if [ ! -d ./bin ]; then
+	mkdir ./bin
+fi
+if [ ! -e ./game_editor/main ]; then
+	echo "Game editor binary wasn't built!"
+	exit
+fi
+cp ./game_editor/main ./bin/editor
+cc *.c  -g -lm -o main
+if [ ! -e ./main ]; then
+	echo "Main game binary wasn't built!"
+	exit
+fi
+mv main bin/game
